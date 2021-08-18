005e84411d12e014fb517032bfc22ab4e7732634
Olivier Penhoat <openhoat@gmail.com>
2021-08-18 14:14:37 +0200

    Add duplicate counter test

---
+++ src/test/duplicate-counter.test.js
@@ -0,0 +1,25 @@
+import { basename } from 'path'
+
+/**
+ * @group sync
+ * @group easy
+ */
+
+const modName = basename(__filename, '.test.js')
+
+describe('duplicate counter', () => {
+  describe('countDuplicate', () => {
+    let countDuplicate
+    beforeAll(async () => {
+      ;({ countDuplicate } = await import(`../main/${modName}`))
+    })
+    test('should return the number of duplicates given an array of values', () => {
+      // Given
+      const ar = [5, 3, 3, 5, 1, 4, 7]
+      // When
+      const result = countDuplicate(ar)
+      // Then
+      expect(result).toBe(2)
+    })
+  })
+})
