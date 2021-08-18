d5553ae7b0a2e1f8a5af01fe1c777735c0db4797
Olivier Penhoat <openhoat@gmail.com>
2021-08-18 18:23:45 +0200

    Add compute cart test

---
+++ src/test/compute-cart.test.js
@@ -0,0 +1,34 @@
+import { basename } from 'path'
+
+/**
+ * @group sync
+ * @group easy
+ */
+
+const modName = basename(__filename, '.test.js')
+
+describe('compute cart', () => {
+  describe('computeCart', () => {
+    let computeCart
+    beforeAll(async () => {
+      ;({ computeCart } = await import(`../main/${modName}`))
+    })
+    test('should return the total price given cart items', () => {
+      // Given
+      // cart content : 3 lines and 2 lines, each line is : qty * unit price
+      const cartContent = `3
+3 47
+6 78
+1 123
+2
+6 33
+3 78
+`
+      const exptectedResult = 3 * 47 + 6 * 78 + 123 + 6 * 33 + 3 * 78
+      // When
+      const result = computeCart(cartContent)
+      // Then
+      expect(result).toBe(exptectedResult)
+    })
+  })
+})
