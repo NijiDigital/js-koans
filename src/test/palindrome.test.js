946d024b14ba986a702dd6a8bcc56bb7b65cd2e0
Olivier Penhoat <openhoat@gmail.com>
2021-08-18 16:15:15 +0200

    Add palindrome test

---
+++ src/test/palindrome.test.js
@@ -0,0 +1,31 @@
+import { basename } from 'path'
+
+/**
+ * @group sync
+ * @group easy
+ */
+
+const modName = basename(__filename, '.test.js')
+
+describe('palindrome', () => {
+  describe('isPalindrome', () => {
+    let isPalindrome
+    beforeAll(async () => {
+      ;({ isPalindrome } = await import(`../main/${modName}`))
+    })
+    test.each([
+      [true, 'radar'],
+      [false, 'maison'],
+      [false, 'table'],
+      [true, ' essayasse'],
+      [true, 'Ressasser'],
+      [true, 'rêver'],
+      [true, 'réifier'],
+    ])('should return %p given %p', (expectedResult, s) => {
+      // When
+      const result = isPalindrome(s)
+      // Then
+      expect(result).toBe(expectedResult)
+    })
+  })
+})
