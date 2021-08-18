ef7dd722c3fe280480eb9bb6bfc8299fb21e3b50
Olivier Penhoat <openhoat@gmail.com>
2021-08-18 16:23:03 +0200

    Add second cheapest test

---
+++ src/test/second-cheapest.test.js
@@ -0,0 +1,31 @@
+import { basename } from 'path'
+
+/**
+ * @group sync
+ * @group easy
+ */
+
+const modName = basename(__filename, '.test.js')
+
+describe('second cheapest', () => {
+  describe('secondCheapestWineName', () => {
+    let secondCheapestWineName
+    beforeAll(async () => {
+      ;({ secondCheapestWineName } = await import(`../main/${modName}`))
+    })
+    test('should return the second cheapest wine name given a list of wines', () => {
+      // Given
+      const wines = [
+        { name: 'Wine A', price: 8.99 },
+        { name: 'Wine 32', price: 13.99 },
+        { name: 'Wine Cheapy', price: 1.99 },
+        { name: 'Wine Cheapo', price: 1.99 },
+        { name: 'Wine 9', price: 10.99 },
+      ]
+      // When
+      const result = secondCheapestWineName(wines)
+      // Then
+      expect(result).toEqual('Wine A')
+    })
+  })
+})
