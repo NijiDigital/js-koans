cea98bdc71d650e3f37722569a0425d408fb8622
Olivier Penhoat <openhoat@gmail.com>
2021-08-19 10:22:18 +0200

    Add each async test

---
+++ src/test/each-async.test.js
@@ -0,0 +1,48 @@
+import { basename } from 'path'
+
+/**
+ * @group async
+ * @group easy
+ */
+
+const modName = basename(__filename, '.test.js')
+
+describe('each async', () => {
+  describe('eachAsync', () => {
+    let eachAsync
+    beforeAll(async () => {
+      ;({ eachAsync } = await import(`../main/${modName}`))
+      jest.useFakeTimers()
+    })
+    afterEach(() => {
+      jest.clearAllTimers()
+    })
+    afterAll(() => {
+      jest.useRealTimers()
+    })
+    test('should iterate through loop and execute async function', async () => {
+      // Given
+      const list = Array.from(Array(3)).map((__, index) => index + 1)
+      const result = []
+      const addLater = (value) =>
+        new Promise((resolve) => {
+          setTimeout(() => {
+            result.unshift(value)
+            resolve()
+          }, value * 1000)
+        })
+      // When
+      const promise = eachAsync(list, (value) => {
+        try {
+          return addLater(value)
+        } finally {
+          jest.advanceTimersByTime(value * 1000)
+        }
+      })
+      await promise
+      jest.runOnlyPendingTimers()
+      // Then
+      expect(result).toEqual([3, 2, 1])
+    })
+  })
+})
