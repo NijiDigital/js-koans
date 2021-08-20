50c7cb4ca9e33904763507636b991f93c79e487b
Olivier Penhoat <openhoat@gmail.com>
2021-08-20 15:25:10 +0200

    Add truthy test

---
+++ src/test/truthy.test.js
@@ -0,0 +1,49 @@
+import { basename } from 'path'
+
+/**
+ * @group sync
+ * @group supereasy
+ */
+
+const modName = basename(__filename, '.test.js')
+
+describe('truthy', () => {
+  let mod
+  beforeAll(async () => {
+    mod = await import(`../main/${modName}`)
+  })
+  describe('getAllFalsies', () => {
+    let getAllFalsies
+    beforeAll(() => {
+      ;({ getAllFalsies } = mod)
+    })
+    test('should return a list of all possible truthy values', () => {
+      // When
+      const result = getAllFalsies()
+      // Then
+      const uniqFalsies = new Set(result)
+      expect(result.length).toBe(6)
+      expect(uniqFalsies.size).toBe(result.length)
+      result.forEach((value) => {
+        expect(value).toBeFalsy()
+      })
+    })
+  })
+  describe('getAllTruthies', () => {
+    let getAllTruthies
+    beforeAll(() => {
+      ;({ getAllTruthies } = mod)
+    })
+    test('should return a list of all possible truthy values', () => {
+      // When
+      const result = getAllTruthies()
+      // Then
+      const uniqTruthies = new Set(result)
+      expect(result.length).toBe(7)
+      expect(uniqTruthies.size).toBe(result.length)
+      result.forEach((value) => {
+        expect(value).toBeTruthy()
+      })
+    })
+  })
+})
