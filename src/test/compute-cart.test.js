8342af4ef692332242016639ab85c73c2be99b3c
Olivier Penhoat <openhoat@gmail.com>
2021-09-07 16:39:01 +0200

    Add compute cart usecase

--- src/test/compute-cart.test.js
+++ src/test/compute-cart.test.js
@@ -7,28 +7,50 @@ import { basename } from 'path'
 
 const modName = basename(__filename, '.test.js')
 
-describe('compute cart', () => {
-  describe('computeCart', () => {
-    let computeCart
-    beforeAll(async () => {
-      ;({ computeCart } = await import(`../main/${modName}`))
-    })
-    test('should return the total price given cart items', () => {
-      // Given
-      // cart content : 3 lines and 2 lines, each line is : qty * unit price
-      const cartContent = `3
+const usecases = [
+  {
+    // cart content : 3 lines and 2 lines, each line is : qty * unit price
+    cartContent: `3
 3 47
 6 78
 1 123
 2
 6 33
 3 78
-`
-      const exptectedResult = 3 * 47 + 6 * 78 + 123 + 6 * 33 + 3 * 78
-      // When
-      const result = computeCart(cartContent)
-      // Then
-      expect(result).toBe(exptectedResult)
+`,
+    expectedResult: 3 * 47 + 6 * 78 + 123 + 6 * 33 + 3 * 78,
+  },
+  {
+    cartContent: `1
+4 8
+5
+1 45
+3 12
+2 9
+3 24
+11 3
+2
+10 4
+3 120
+`,
+    expectedResult: 4 * 8 + 1 * 45 + 3 * 12 + 2 * 9 + 3 * 24 + 11 * 3 + +10 * 4 + 3 * 120,
+  },
+]
+
+describe('compute cart', () => {
+  describe('computeCart', () => {
+    let computeCart
+    beforeAll(async () => {
+      ;({ computeCart } = await import(`../main/${modName}`))
     })
+    test.each(usecases)(
+      'should return $expectedResult as the total price given cart items $cartContent',
+      ({ cartContent, expectedResult }) => {
+        // When
+        const result = computeCart(cartContent)
+        // Then
+        expect(result).toBe(expectedResult)
+      },
+    )
   })
 })
