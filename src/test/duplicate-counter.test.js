03fefef6b72912730e2450c7dc06288ab745816f
Olivier Penhoat <openhoat@gmail.com>
2021-09-09 17:34:41 +0200

    Improve duplicate counter test

--- src/test/duplicate-counter.test.js
+++ src/test/duplicate-counter.test.js
@@ -13,13 +13,18 @@ describe('duplicate counter', () => {
     beforeAll(async () => {
       ;({ countDuplicate } = await import(`../main/${modName}`))
     })
-    test('should return the number of duplicates given an array of values', () => {
-      // Given
-      const ar = [5, 3, 3, 5, 1, 4, 7]
-      // When
-      const result = countDuplicate(ar)
-      // Then
-      expect(result).toBe(2)
-    })
+    test.each([
+      { ar: [5, 3, 3, 5, 1, 4, 7], expectedResult: 2 },
+      { ar: [5, 3, 3, 5, 1, 1, 1, 4, 7], expectedResult: 4 },
+      { ar: [5, 5, 5, 5], expectedResult: 3 },
+    ])(
+      'should return $expectedResult as number of duplicates given an array of values of $ar',
+      ({ ar, expectedResult }) => {
+        // When
+        const result = countDuplicate(ar)
+        // Then
+        expect(result).toBe(expectedResult)
+      },
+    )
   })
 })
