e1cf40a6c835c5b6baeb9a8f5990817e84841c28
Olivier Penhoat <openhoat@gmail.com>
2021-09-15 18:07:37 +0200

    Improve tests

--- src/test/alphabet.test.js
+++ src/test/alphabet.test.js
@@ -19,23 +19,19 @@ describe('alphabet', () => {
       // Then
       expect(result).toEqual('abcdefghijklmnopqrstuvwxyz')
     })
-    test("should return a string with letters from 'c' to 'x'", () => {
-      // When
-      const result = buildLetters('c', 'x')
-      // Then
-      expect(result).toEqual('cdefghijklmnopqrstuvwx')
-    })
-    test("should return a string with letters from 'g' to 'm'", () => {
-      // When
-      const result = buildLetters('g', 'm')
-      // Then
-      expect(result).toEqual('ghijklm')
-    })
-    test("should return a string with letters from 'o' to 'z'", () => {
-      // When
-      const result = buildLetters('o')
-      // Then
-      expect(result).toEqual('opqrstuvwxyz')
-    })
+    test.each([
+      { expectedResult: 'cdefghijklmnopqrstuvwx', from: 'c', to: 'x' },
+      { expectedResult: 'ghijklm', from: 'g', to: 'm' },
+      { expectedResult: 'opqrstuvwxyz', from: 'o', to: undefined },
+      { expectedResult: 'ABCDEFGHIJK', from: 'A', to: 'K' },
+    ])(
+      'should return $expectedResult given from=$from and to=$to',
+      ({ expectedResult, from, to }) => {
+        // When
+        const result = buildLetters(from, to)
+        // Then
+        expect(result).toEqual(expectedResult)
+      },
+    )
   })
 })
