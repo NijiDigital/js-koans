f8c3762f5915dc25a1f082a2fd9f415fa0f7c6a4
Olivier Penhoat <openhoat@gmail.com>
2021-09-16 17:35:23 +0200

    Improve tests

--- src/test/each-async.test.js
+++ src/test/each-async.test.js
@@ -12,36 +12,23 @@ describe('each async', () => {
     let eachAsync
     beforeAll(async () => {
       ;({ eachAsync } = await import(`../main/${modName}`))
-      jest.useFakeTimers()
-    })
-    afterEach(() => {
-      jest.clearAllTimers()
-    })
-    afterAll(() => {
-      jest.useRealTimers()
     })
     test('should iterate through loop and execute async function', async () => {
       // Given
-      const list = Array.from(Array(3)).map((__, index) => index + 1)
+      const list = Array.from({ length: 3 }, (__, index) => index + 1)
       const result = []
-      const addLater = (value) =>
-        new Promise((resolve) => {
+      const addLater = (value) => {
+        return new Promise((resolve) => {
           setTimeout(() => {
             result.unshift(value)
             resolve()
           }, value * 1000)
         })
+      }
       // When
-      const promise = eachAsync(list, (value) => {
-        try {
-          return addLater(value)
-        } finally {
-          jest.advanceTimersByTime(value * 1000)
-        }
-      })
-      await promise
-      jest.runOnlyPendingTimers()
+      const promiseFactory = () => eachAsync(list, (value) => addLater(value))
       // Then
+      await expect(promiseFactory).toFulfillAfter(1000 + 2000 + 3000)
       expect(result).toEqual([3, 2, 1])
     })
   })
